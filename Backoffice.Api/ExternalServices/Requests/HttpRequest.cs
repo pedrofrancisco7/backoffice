4c101c93baf4f65bd17c2888690c1cff05922f47
pedrorodrigues <pedro.francisco@msn.com>
2023-02-21 15:36:32 -0300

    External Services

---
+++ Backoffice.Api/ExternalServices/Requests/HttpRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ExternalServices.Requests
+{
+	public class HttpRequest
+	{
+        public async Task<HttpResponseMessage> GetCep(string cep)
+        {
+            try
+            {
+                using (var http = GetHttpClient("https://viacep.com.br/ws/"))
+                {                    
+                    return await http.GetAsync($"{cep}/json/");
+                }
+            }
+            catch (Exception)
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+            }
+
+        }
+
+
+        private static HttpClient GetHttpClient(string baseref)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(baseref);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            return httpClient;
+        }
+    }
+}
+
---
+++ Backoffice.Api/ExternalServices/Services/ExternalServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Backoffice.Domain.Entities;
+using Backoffice.Domain.Interfaces;
+using ExternalServices.Requests;
+using Newtonsoft.Json;
+
+namespace ExternalServices.Services;
+
+public class ExternalServices : IExternalServices
+{
+    public async Task<EnderecoEntity?> GetEndereco(string cep)
+    {
+        try
+        {
+            var response = await new HttpRequest().GetCep(cep);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+
+            var endereco = JsonConvert.DeserializeObject<EnderecoEntity>(result) ?? new EnderecoEntity();
+            return endereco;
+
+        }
+        catch (Exception ex)
+        {
+            //throw new Exception(ex.Message, ex);
+            return new EnderecoEntity { erro = "true" };
+        }
+    }
+}
+
